cd32f4d70ecb6c8cc3b4014bd0ca46662f215a39
krishna-420 <72797251+krishna-420@users.noreply.github.com>
2020-11-27 16:24:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var options={
+	isStatic:true,
+	restitution:0,
+	friction :1,
+}
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if (keyCode===32){
+		Matter.Body.setPosition(stoneObh.Body, {x:235,y:420})
+		launcherObject.attach(stoneObj.Body);
+	}
+}
+
+function detectollision(Lstone,Lmango){
+	mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+
+	var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r)
+	{
+		Matter.Body.setStatic(lmango.body,false);
+	}
+}
+
